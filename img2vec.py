08303852c7d581738a2a3d6565cc9c881d58f9bd
louie <louiezzang@gmail.com>
2022-08-31 16:19:06 -0700

    Added readme

--- img2vec.py
+++ img2vec.py
@@ -11,9 +11,16 @@ from PIL import Image
 class Img2Vec(nn.Module):
     def __init__(self):
         super().__init__()
-        self.model = torchvision.models.resnet18(pretrained=True)
-        self.model.eval()
-        self.layer = self.model._modules.get("avgpool")
+        # Pretrained model for extracting embedding.
+        self.pt_model = torchvision.models.resnet18(pretrained=True)
+        # Finetuned model for predict class label.
+        self.ft_model = torchvision.models.resnet18(pretrained=True)
+
+        self.pt_model.eval()
+        self.ft_model.eval()
+
+        # Embedding layer from the pretrained model.
+        self.layer = self.pt_model._modules.get("avgpool")
 
     def forward(self, x):
         my_embedding = torch.zeros(1, 512, 1, 1)
@@ -22,55 +29,58 @@ class Img2Vec(nn.Module):
             my_embedding.copy_(o.data)
 
         h = self.layer.register_forward_hook(copy_data)
-        prediction = self.model(x)
+        _ = self.pt_model(x)
+        prediction = self.ft_model(x)
 
         h.remove()
 
-        # return self.model(x)
-        # return torch.zeros(1, 512, 1, 1).view(-1, 512).squeeze(0)
-        # return my_embedding.view(-1, 512).squeeze(0)
-        return torch.tensor(my_embedding.numpy()[0, :, 0, 0]), prediction.squeeze(0)
+        return prediction.squeeze(0), torch.tensor(my_embedding.numpy()[0, :, 0, 0])
 
 
 if __name__ == "__main__":
-    example_input = torch.rand(1, 3, 224, 224)
+    # Download class labels.
+    import urllib
+    label_url = "https://raw.githubusercontent.com/pytorch/hub/master/imagenet_classes.txt"
+    class_labels = urllib.request.urlopen(label_url).read().decode("utf-8").splitlines()
+    assert len(class_labels) == 1000
+    print(class_labels)
+
+    dummy_input = torch.rand(1, 3, 224, 224)
 
     img2vec = Img2Vec()
-    emb, pred = img2vec(example_input)
+    # pred, emb = img2vec(dummy_input)
     # print(emb)
 
-    traced_model = torch.jit.trace(img2vec, example_input)
-    out, pred = traced_model(example_input)
-    # print(out)
-
-    # input_tensor = ct.TensorType(name="my_input", shape=example_input.shape)
-    # cml_model = ct.convert(traced_model, inputs=[input_tensor], source="pytorch")
+    traced_model = torch.jit.trace(img2vec, dummy_input)
+    pred, emb = traced_model(dummy_input)
+    # print(emb)
 
     cml_model = ct.convert(traced_model,
                            inputs=[ct.ImageType(name="image", color_layout="RGB", scale=1.0/255.0/0.226,
                                                 bias=(-0.485/0.226, -0.456/0.226, -0.406/0.226),
-                                                shape=example_input.shape)],
-                           # outputs=[ct.TensorType(name="emb", shape=(512,), dtype=np.float16),
-                           #          ct.TensorType(name="pred", shape=(1000,), dtype=np.float16)
-                           #          ],
+                                                shape=dummy_input.shape)],
+                           classifier_config=ct.ClassifierConfig(class_labels),
                            source="pytorch"
                            )
 
-    # cml_model = ct.convert(traced_model, inputs=[ct.ImageType(shape=example_input.shape)])
-    # print(cml_model)
+    # Change the output names.
+    spec = cml_model.get_spec()
+    ct.utils.rename_feature(spec, spec.description.output[0].name, "pred", rename_outputs=True)
+    ct.utils.rename_feature(spec, spec.description.output[1].name, "emb", rename_outputs=True)
+    cml_model = ct.models.MLModel(spec)
 
     cml_model.save("img2vec.mlmodel")
 
     IS_MACOS = sys.platform == "darwin"
 
-    test_img = Image.open("./LW3EZTS_0001_1.webp")
+    test_img = Image.open("sample_img.webp")
     test_img = test_img.resize((224, 224))
 
     if IS_MACOS:
         loaded_model = ct.models.MLModel("img2vec.mlmodel")
         pred_output = loaded_model.predict({"image": test_img})
         print("=" * 50)
-        print(pred_output)
+        # print(pred_output)
 
         for k, v in pred_output.items():
-            print(k, v.shape)
+            print(f"{k}: {v}")
